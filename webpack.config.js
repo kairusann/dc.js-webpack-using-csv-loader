46886309075abb06d7ea307c794f208a39514408
Kyle Zhong <kyle.zh@gmail.com>
2020-04-13 12:41:56 -0400

    added webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   devtool: 'source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'DC.js Demo'
